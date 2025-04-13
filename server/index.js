aefdb3a821684d564aaa7c2fafaf1ce0b1a7745d
Harsh Chouhan <harsh.chouhan.0918@gmail.com>
2025-04-13 17:15:30 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,9 @@ app.get('/', (req, res) => {
 
 const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
